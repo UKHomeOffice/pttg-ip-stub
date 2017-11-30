3288200915238148ca5be8e4b62cf944e833084c
Lucy Bennett <lucy.bennett@digital.homeoffice.gov.uk>
2017-11-30 13:49:02 +0000

    wire up custom media type to product HAL (#18)

--- src/main/java/uk/gov/digital/ho/pttg/ApplicationConfiguration.java
+++ src/main/java/uk/gov/digital/ho/pttg/ApplicationConfiguration.java
@@ -4,18 +4,25 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 
 @Configuration
-public class ApplicationConfiguration {
+public class ApplicationConfiguration  {
 
+
+    private static final String SPRING_HATEOAS_OBJECT_MAPPER = "_halObjectMapper";
 
     @Autowired
-    public ApplicationConfiguration(ObjectMapper objectMapper) {
-        initialiseObjectMapper(objectMapper);
-    }
+    @Qualifier(SPRING_HATEOAS_OBJECT_MAPPER)
+    private ObjectMapper springHateoasObjectMapper;
 
     public static ObjectMapper initialiseObjectMapper(final ObjectMapper m) {
         m.setDateFormat(new SimpleDateFormat("yyyy-MM-dd"));
@@ -23,6 +30,22 @@ public class ApplicationConfiguration {
         m.enable(SerializationFeature.INDENT_OUTPUT);
         m.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         return m;
+    }
+
+    @Primary
+    @Bean
+    public ObjectMapper objectMapper() {
+        final ObjectMapper objectMapper = springHateoasObjectMapper;
+        initialiseObjectMapper(objectMapper);
+        return objectMapper;
+    }
+
+    @Bean
+    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+        MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter();
+        jsonConverter.setObjectMapper(objectMapper());
+        jsonConverter.setSupportedMediaTypes(Arrays.asList(new MediaType("application", "hal+json"), new MediaType("application", "vnd.hmrc.P1.0+json")));
+        return jsonConverter;
     }
 
 }
